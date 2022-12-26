cba419d3883eae6a44ae10e2c8afa27220d679ca
Shagor <mdshagormahamud028@gmail.com>
2022-12-26 16:52:52 +0600

    added by banner section

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,13 +1,17 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Slider1 from '../../Asstes/Slider img/Slider1.png'
+import Slider2 from '../../Asstes/Slider img/Slider2.png'
+import Slider3 from '../../Asstes/Slider img/Slider3.png'
 import styled from 'styled-components';
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: coral;
+    justify-content: space-between;
     position: relative;
+    overflow: hidden;
 `
 const Arrow = styled.div`
     width: 50px;
@@ -25,16 +29,93 @@ const Arrow = styled.div`
     cursor: pointer;
     margin: auto;
     opacity: 0.5;
+    z-index: 2;
+    
+`
+const Wrapper = styled.div`
+height: 100%;
+display: flex;
+transition: all 1.5s ease;
+transform: translateX(${(props) => props.slideIndex * -100}vw);
+`
+const Slide = styled.div`
+width:100vw;
+height: 100vh;
+display:flex;
+align-items: center;
+background-color:#${props => props.bg};
+`
+const ImgContainer = styled.div`
+flex:1
+`
+const Image = styled.img`
+height:100%
 
 `
+const InfoContainer = styled.div`
+flex:1;
+padding:50px;
+`
+const Title = styled.h1`
+font-size: 70px;
+;
+`
+const Desc = styled.p`
+margin: 50px 0px;
+font-size: 20px;
+letter-spacing: 3px;
+`
+const Button = styled.button`
+padding: 10px 25px;
+font-size: 20px;
+background-color: teal;
+border: none;
+color: white;
+cursor: pointer;
+border-radius: 5px;
+`
+
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0)
+    const [slideItems, setSlideItems] = useState([])
+
+    useEffect(() => {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => setSlideItems(data))
+    }
+        , [])
+    const handleClick = (direction) => {
+        if (direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+        }
+
+    }
     return (
         <Container>
-            <Arrow direction="left">
-                <ArrowLeftOutlined />
+            <Arrow direction="left" onClick={() => handleClick("left")}>
+                <ArrowLeftOutlined style={{ fontSize: "50px", color: "teal" }} />
             </Arrow>
-            <Arrow direction="right">
-                <ArrowRightOutlined />
+            <Wrapper slideIndex={slideIndex}>
+                {
+                    slideItems.map((item) => (
+                        <Slide bg={item.bg}>
+                            <ImgContainer>
+                                <Image src={item?.image} />
+                            </ImgContainer>
+                            <InfoContainer>
+                                <Title>{item?.title}</Title>
+                                <Desc>{item?.description}</Desc>
+                                <Button>SHOW NOW</Button>
+                            </InfoContainer>
+                        </Slide>
+                    ))
+                }
+            </Wrapper>
+            <Arrow direction="right" onClick={() => handleClick("right")}>
+                <ArrowRightOutlined style={{ fontSize: "50px", color: "teal" }} />
             </Arrow>
         </Container>
     );
